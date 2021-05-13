87837bbb333f3f7be6295a176781828891cffff0
Duy-above-average <nguyenanhduy0400@gmail.com>
2021-05-13 09:43:23 +0700

    Show products by categories
    GUI thoi nha quy di

--- controllers/products.route.js
+++ controllers/products.route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const categoryModel = require("../models/category.model");
 const productsModel = require("../models/products.model");
 const { paginate } = require('./../config/default.json');
 const router = express.Router();
@@ -43,6 +44,8 @@ router.get("/byCat1/:id", async function (req, res) {
   const offset = (page - 1) * paginate.limit;
 
   let allProductsFromCate1 = await productsModel.getAllProductsByCate1Id(cate1Id, offset);
+  let cate1Name = await categoryModel.getCateName1ById(cate1Id);
+  let allCategories = await productsModel.allCategories();
 
   if (allProductsFromCate1 !== null)
     Promise.all(await allProductsFromCate1.map(async product => {
@@ -59,10 +62,62 @@ router.get("/byCat1/:id", async function (req, res) {
       nextPage: +page + 1,
       firstPage: +page === 1,
       lastPage: +page === nPages,
+      page_numbers,
+      cate1Name,
+      cate1Id,
+      allCategories,
   });
 });
 //Xem ds sản phẩm theo danh mục 2
-router.get("/byCat2", async function (req, res) {});
+router.get("/byCat2/:id", async function (req, res) {
+  let cate2Id = +req.params.id;
+  let cate1Id = await categoryModel.getCate1IdFromCate2Id(cate2Id);
+
+  var page = req.query.page || 1;
+  if (page < 1) page = 1;
+
+  const total = await productsModel.countAllByCate2(cate2Id);
+  let nPages = Math.floor(total / paginate.limit);
+  if (total % paginate.limit > 0) nPages++;
+
+  const page_numbers = [];
+  for (var i = 1; i <= nPages; i++) {
+    page_numbers.push({
+      value: i,
+      isCurrentPage: i === +page,
+    });
+  }
+
+  const offset = (page - 1) * paginate.limit;
+
+  let allProductsFromCate2 = await productsModel.getAllProductsByCate2Id(cate2Id, offset);
+  let cate1Name = await categoryModel.getCateName1ById(cate1Id);
+  let cate2Name = await categoryModel.getCateName2ById(cate2Id);
+  let allCategories = await productsModel.allCategories();
+
+  if (allProductsFromCate2 !== null)
+    await allProductsFromCate2.map(async product => {
+        let img = await productsModel.getImages(+product.maso);
+        product.hinhanh = img[0].link;
+        return product;
+    });
+
+  res.render("../views/vwProducts/byCat2.hbs", {
+    layout: 'main.hbs',
+    products: allProductsFromCate2,
+    onlyOne: nPages < 1,
+    prevPage: +page - 1,
+    nextPage: +page + 1,
+    firstPage: +page === 1,
+    lastPage: +page === nPages,
+    page_numbers,
+    cate1Name,
+    cate2Name,
+    cate1Id,
+    cate2Id,
+    allCategories,
+});
+});
 
 //Search sản phẩm
 router.post("/search", async function (req, res) {});
